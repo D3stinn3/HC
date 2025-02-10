90f4d87598720167277520fb541ab2c902b3880d
D3stinn3 <paynedestinne@gmail.com>
2025-02-10 14:11:37 +0300

    Host Config

--- HCUser/views.py
+++ HCUser/views.py
@@ -3,15 +3,23 @@ from ninja_extra import NinjaExtraAPI, api_controller, http_get
 from ninja_extra.permissions import IsAuthenticated
 
 
-# Create your views here.
+"""NinjaExtra API FOR HomeChoice"""
+
 api = NinjaExtraAPI()
 
-@api.get("/hello", tags=['Basic'])
+"""Intrinsic Data Handling"""
+
+@api.get("/hello", tags=['HCUser'])
 def hello(request, name: str = "World"):
     return {"message": f"Hello, {name}!"}
 
-@api.get("/add", tags=['Basic'])
+@api.get("/add", tags=['HCUser'])
 def add(request, a: int, b: int):
     return {"result": a + b}
+
+"""Extrinsic Data Handling"""
+
+
+
 
 
