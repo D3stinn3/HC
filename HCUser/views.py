4d0867ce9abe77911b2a180ce20fc8f4855bab22
D3stinn3 <paynedestinne@gmail.com>
2025-02-23 16:38:29 +0300

    CSRF Set

--- HCUser/views.py
+++ HCUser/views.py
@@ -47,10 +47,8 @@ def add(request, a: int, b: int):
 
 """ CSRF Token Management """
 
-from django.http import JsonResponse
-from django.middleware.csrf import get_token
-
 @api.get("/get_csrf_token", response=ResponseSchema, tags=["csrf"])
+@ensure_csrf_cookie
 def get_csrf_token_api(request, email: str):
     """
     Retrieves CSRF token from Redis using user.email or generates a new one.
@@ -61,14 +59,13 @@ def get_csrf_token_api(request, email: str):
         csrf_token = get_token(request)  # Generate new CSRF token
         csrf_cache.set(f"csrf_token:{email}", csrf_token, timeout=None)  # Store indefinitely
 
-    response = JsonResponse({
+    response_data = {
         "success": True,
         "message": "CSRF token retrieved successfully",
         "data": {"csrf_token": csrf_token}
-    })
-    response.set_cookie('csrftoken', csrf_token)  # Set the CSRF cookie
-    return response
+    }
 
+    return JsonResponse(response_data)
 
 
 @api.post("/store_csrf_token", response=ResponseSchema, tags=["csrf"])
@@ -128,7 +125,7 @@ def signup(request, payload: SignupSchema):
 """User Login"""
 
 @api.post("/login/homechoice-user", response=ResponseSchema, tags=["user"])
-@csrf_protect
+@ensure_csrf_cookie
 def user_login(request, payload: LoginSchema):
     """
     Logs in users and retrieves or stores CSRF token.
